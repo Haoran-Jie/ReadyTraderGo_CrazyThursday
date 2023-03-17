46573522b94547ff54f482f204612e90917ddeae
PeterHUistyping <2447276617@qq.com>
2023-03-17 23:07:36 +0000

    Release

--- initiate.sh
+++ initiate.sh
@@ -1,7 +1,7 @@
 sudo rm -rf build
 echo ----remove the cache, done!----
-cmake -DCMAKE_BUILD_TYPE=Debug -B build
-cmake --build build --config Debug
+cmake -DCMAKE_BUILD_TYPE=Release -B build
+cmake --build build --config Release
 cp build/autotrader .
 echo ---- Ready to run! -----
 python3 rtg.py run autotrader
